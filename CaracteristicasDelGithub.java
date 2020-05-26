5d9bbc5d317878f16caa0bbd39221a656336a750
RTN97 <RTN5>
2020-05-26 02:35:45 -0500

    Github

---
+++ CaracteristicasDelGithub.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caracteristicas.del.github;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class CaracteristicasDelGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner objeto=new Scanner(System.in);
+        System.out.println("Que es github");
+        String queesgithub = objeto.nextLine();
+        
+        System.out.println("Caracteristicas del Github");
+        System.out.println("Caracteristica 1");
+        String caracteristica1=objeto.nextLine();
+        
+        System.out.println("Caracteristica 2");
+        String caracteristica2=objeto.nextLine();
+        
+        System.out.println("Caracteristica 3");
+        String caracteristica3=objeto.nextLine();
+        
+        System.out.println("Caracteristica 4");
+        String caracteristica4=objeto.nextLine();
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
